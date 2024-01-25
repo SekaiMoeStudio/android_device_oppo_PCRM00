903cbe1618be8946e29a879080c0ffed3adcf5d1
ワールドキュート <153579552+SekaiMoe@users.noreply.github.com>
2024-01-26 02:05:04 +0800

    PCRM00: use partitions.sh to suitable for Android

--- partitions.sh
+++ partitions.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
 set -e
-call_cat="/bin/adb shell cat"
-call_ls="/bin/adb shell ls"
-name_base="/dev/block/bootdevice"
-if [[ -n $1 ]]; then
-  block_name=$(${call_ls} -la ${name_base}/by-name/$1 2>/dev/null | awk -F " -> /dev/block/" '{print $2}') # 将 分区可读名称 转换为 块设备名称
-  if [[ -n ${block_name} ]]; then
-    ${call_cat} /proc/partitions | grep ${block_name} | awk -F " " '{print $3 * 1024}' # 输出单位为 Byte
-  else
-    echo "Error: partition $1 not found"
-  fi
+
+if uname -a | grep Android >/dev/null; then
+    if [[ $EUID -ne 0 ]]; then
+        echo "Please use root user to execute this script"
+        exit 1
+    fi
+    export call_cat="/bin/cat"
+    export call_ls="/bin/ls"
 else
-  echo "Usage: $0 [partition name]"
+    export call_cat="/bin/adb shell su -c cat"
+    export call_ls="/bin/adb shell su -c ls"
+fi
+
+export name_base="/dev/block/bootdevice"
+
+if [[ -n $1 ]]; then
+    block_name=$(${call_ls} -la ${name_base}/by-name/$1 2>/dev/null | awk -F " -> /dev/block/" '{print $2}')
+    if [[ -n ${block_name} ]]; then
+        ${call_cat} /proc/partitions | grep ${block_name} | awk -F " " '{print $3 * 1024}'
+    else
+        echo "Error: partition $1 not found"
+    fi
+else
+    echo "Usage: $0 [partition name]"
+    exit 127
 fi
